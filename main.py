2c747d4ab565f3985e66c714490b0b3bfefd2719
Shiina <sergio5203@gmail.com>
2024-09-24 18:32:55 -0300

    Ship rendered on Screen

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from constants import *
+from player import Player
+
+pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 def main():
-    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        clock.tick(60)
+        dt = clock.tick(60)/1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
