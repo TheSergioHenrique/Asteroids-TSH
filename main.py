cb62cf020cbc51cb3b1ac76376358e50fb0d76fa
Shiina <sergio5203@gmail.com>
2024-10-18 13:51:26 -0300

    Added Recoil.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main():
 
         pygame.display.flip()
 
-        #Limitador de Framerate
+        #Limitador de Framerate.Vale dizer que o jogo todo se baseia nesse clock.
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
