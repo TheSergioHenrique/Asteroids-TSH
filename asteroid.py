df0cca077e5f44f5a950e69890eb5cdc17a93d85
Shiina <sergio5203@gmail.com>
2024-10-19 12:27:28 -0300

    Added splitting

--- asteroid.py
+++ asteroid.py
@@ -12,3 +12,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        #Randomizar o ângulo da separação dos Asteroides
+        random_angle = random.uniform(20,50)
+
+        ast1= self.velocity.rotate(random_angle)
+        ast2= self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x,self.position.y, new_radius)
+        asteroid.velocity = ast1 * 1.2
+        asteroid = Asteroid(self.position.x,self.position.y, new_radius)
+        asteroid.velocity = ast2 * 1.2
